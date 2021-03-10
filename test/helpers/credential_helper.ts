54a559e91098d04897cdad3c2c8d9dca0895e404
Mus Radi <68963994+innovatechs@users.noreply.github.com>
2021-03-10 17:08:06 -0500

    Update credential_helper.ts

--- test/helpers/credential_helper.ts
+++ test/helpers/credential_helper.ts
@@ -21,6 +21,7 @@ class CredentialHelper extends Helper {
 
   getEnv(name) {
     const envName = name.toString().toUpperCase().replace(/ /g,"_");
+    console.log(process.env.GCKEY_USER)
     const val = process.env[envName];
     this.references[name] = val;
     return {val, what: envName};
